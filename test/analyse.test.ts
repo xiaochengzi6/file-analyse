e22e31148b3d83fd3a62060e4009e15fc1f5c6f8
小橙子 <a2742217795@hotmail.com>
2023-04-16 14:57:39 +0800

    test: 修改 analyse 测试文件

--- test/analyse.test.ts
+++ test/analyse.test.ts
@@ -1,22 +1,22 @@
 import { describe, expect, it } from 'vitest'
-import { analyse } from '../src/proxy/analyse';
+import { FileAnalyse } from '../src/proxy/analyse';
 import { getBabelParser } from '../src/babel';
+
+const analyse = new FileAnalyse()
 
 const getResult = (code) => {
   const parser = getBabelParser()
   const ast = parser.parse(code).program
-  return analyse(ast)
+  analyse.update(ast)
+
+  return analyse
 }
 
 describe("analyse", () => {
   it("checkout exports", () => {
     expect(getResult('export default foo')).toEqual({
-      imports: {
-        $type: 'imports'
-      },
+      imports: {},
       exports: {
-        $type: 'exports',
-
         default: expect.objectContaining({
           name: 'default',
           localName: 'foo',
@@ -26,25 +26,11 @@ describe("analyse", () => {
     })
 
     expect(getResult('export const a = 1')).toEqual({
-      imports: {
-        $type: 'imports'
-      },
+      imports: {},
       exports: {
-        $type: 'exports',
-        a: {
+        a: expect.objectContaining({
           localName: 'a',
-          expression: expect.objectContaining({
-            "type": "VariableDeclaration",
-            "kind": "const"
-          }),
-          _node: expect.objectContaining({
-            "type": "ExportNamedDeclaration",
-            "specifiers": [],
-            "source": null,
-            "assertions": [],
-            "exportKind": "value"
-          })
-        }
+        })
       }
     })
 
@@ -53,12 +39,8 @@ describe("analyse", () => {
       export {build}
     `
     expect(getResult(testBuild)).toEqual({
-      imports: {
-        $type: 'imports'
-      },
+      imports: {},
       exports: {
-        $type: 'exports',
-
         build: expect.objectContaining({
           "localName": 'build',
           "exportedName": 'build',
@@ -67,34 +49,45 @@ describe("analyse", () => {
     })
 
     expect(getResult('export * from "filePath"')).toEqual({
+      exports: {},
+      imports: {
+        filePath: expect.objectContaining({
+          from: "filePath",
+          isAll: true,
+          imported: '*',
+        })
+      }
+    })
+
+    expect(getResult('export foo from "FOO"')).toEqual({
       exports: {
-        $type: 'exports'
+        foo: {
+          exportedName: 'foo',
+          localName: 'foo',
+        },
       },
       imports: {
-        $type: 'imports',
-        filePath: expect.objectContaining({
-          source: "filePath",
-          isAll: true,
-          name: '*',
-        })
+        foo: {
+          from: 'FOO',
+          local: 'foo',
+          imported: 'default'
+        }
       }
     })
 
     expect(getResult('export * as FileName from "filePath"')).toEqual({
       exports: {
-        $type: 'exports',
         FileName: expect.objectContaining({
           localName: 'FileName',
           exportedName: 'FileName',
         })
       },
       imports: {
-        $type: 'imports',
         FileName: expect.objectContaining({
-          source: "filePath",
+          from: "filePath",
           // name 是导入文件的名称
-          name: '*',
-          localName: 'FileName',
+          imported: '*',
+          local: 'FileName',
           isAll: true,
         })
       }
@@ -102,51 +95,43 @@ describe("analyse", () => {
 
     expect(getResult('export {bar as foo} from "fileNamePath"')).toEqual({
       imports: {
-        $type: 'imports',
         bar: expect.objectContaining({
-          name: 'foo',
-          localName: 'bar',
-          source: 'fileNamePath'
+          local: 'bar',
+          from: 'fileNamePath'
         })
       },
       exports: {
-        $type: 'exports',
-
         foo: expect.objectContaining({
           "localName": 'bar',
           "exportedName": 'foo',
         })
       }
     })
+
+
   })
 
   it('checkout imports', () => {
     expect(getResult('import bar from "bar"')).toEqual({
       imports: {
-        $type: 'imports',
-        bar: expect.objectContaining({
-          source: 'bar',
-          name: 'default',
-          localName: 'bar',
-        })
+        bar: {
+          from: 'bar',
+          imported: 'default',
+          local: 'bar',
+        }
       },
-      exports: {
-        $type: 'exports'
-      }
+      exports: {}
     })
 
     expect(getResult('import * as bar from "Bar"')).toEqual({
       imports: {
-        $type: 'imports',
-        bar: expect.objectContaining({
-          source: 'Bar',
-          name: '*',
-          localName: 'bar',
-        })
+        bar: {
+          from: 'Bar',
+          imported: '*',
+          local: 'bar',
+        }
       },
-      exports: {
-        $type: 'exports'
-      }
+      exports: {}
     })
   })
 
